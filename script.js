acd10f98d6681e02355db5293e1cc5acfcb67629
“wakchaurekiran” <wakchaurekiranmk@sanjivani.org.in>
2024-03-16 14:05:56 +0530

    major changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var type = new Typed('.typing-text', {
-  strings: ['a teacher', 'a passionate programmer'],
+  strings: ['a teacher', 'a Maker', 'an Innovator'],
   typeSpeed: 120,
   loop: true
 });
